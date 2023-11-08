bda81c87d1c1142b00c4a90bbfa792261b47d611
OlegNalivannij <nalivannijoleg@gmail.com>
2023-11-08 17:00:30 +0200

    add lab2

---
+++ Lab2/Lab2/Lab2.cpp
@@ -0,0 +1,17 @@
+﻿#include "stdio.h"
+#include "windows.h"
+
+
+
+int main()
+{
+	double A , S, P;
+	printf("input A: ");
+	scanf_s("%lf", &A);
+	S = A * A;
+	P = A * 4;
+	printf("P = %lf\n", P);
+	printf("S = %lf\n", S);
+	system("pause");
+	return 0;
+}
